9a2faacda60492ec13adfed32c774ba2a6a4b63a
darrinmn9 <dnagengast9@gmail.com>
2016-02-26 01:16:28 -0800

    created basic class and instance

--- init.py
+++ init.py
@@ -1,4 +1,21 @@
-f = open('myfile.txt')
-print(f.read())
-f.seek(0)
-print(f.read())
+class Circle(object):
+
+    pi = 3.14
+
+    def __init__(self, **params):
+        self.props = params
+        self.radius = params['radius']
+        self.color = params['color']
+        self.area = Circle.pi * (self.radius**2)
+
+    def get_area(self):
+        return self.area
+
+    def set_radius(self, radius):
+        self.radius = radius
+
+    def get_props(self):
+        return self.props
+
+x = Circle(radius=1, color='red')
+print(x.get_props())
