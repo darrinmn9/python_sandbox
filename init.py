909c811318c2c1107175953dd4ded265eecd37d2
darrinmn9 <dnagengast9@gmail.com>
2016-03-05 19:07:34 -0800

    created buy_sell profit algo

--- init.py
+++ init.py
@@ -1,8 +1,17 @@
-from collections import Counter
+def profit(stock_prices):
+    if len(stock_prices) < 2:
+        return None
 
+    min_price = stock_prices[0]
+    profit = stock_prices[1] - stock_prices[0]
 
-l = [1, 2, 2, 2, 2, 3, 3, 3, 1, 2, 1, 12, 3, 2, 32, 1, 21, 1, 223, 1]
+    for price in stock_prices[1:]:
+        min_price = min(min_price, price)
+        if (price - min_price > profit):
+            profit = price - min_price
 
-print(Counter(l))
+    return profit
 
-print(Counter('aabsbsbsbhshhbbsbs'))
+print(profit([12, 11, 15, 3, 10]))
+print(profit([10, 12, 14, 12, 13, 11, 8, 7, 6, 13, 23, 45, 11, 10]))
+print(profit([1]))
