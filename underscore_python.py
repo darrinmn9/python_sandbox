bcd653794a5a1a6dea514a6ecbcdfaa62bd503a7
darrinmn9 <dnagengast9@gmail.com>
2016-03-07 16:41:50 -0800

    init underscore functions

---
+++ underscore_python.py
@@ -0,0 +1,111 @@
+def identity(val):
+    return val
+
+
+def first(array, n):
+    return
+
+
+def last(array, n):
+    return
+
+
+def each(collection, iterator):
+    return
+
+
+def indexOf(array, target):
+    return
+
+
+def filter(collection, test):
+    return
+
+
+def reject(collection, test):
+    return
+
+
+def uniq(array):
+    return
+
+
+def map(collection, iterator):
+    return
+
+
+def pluck(collection, key):
+    return
+
+
+def reduce(collection, iterator, accumulator):
+    return
+
+
+def contains(collection, target):
+    return
+
+
+def every(collection, iterator):
+    return
+
+
+def some(collection, iterator):
+    return
+
+#Objects
+
+
+def extend(obj):
+    return
+
+
+def defaults(obj):
+    return
+
+# function
+
+
+def once(func):
+    return
+
+
+def memoize(func):
+    return
+
+
+def delay(func, wait):
+    return
+# Advanced Collection Operations
+
+
+def shuffle(array):
+    return
+
+
+def invoke(collection, functionOrKey, args):
+    return
+
+
+def sortBy(collection, iterator):
+    return
+
+
+def zip():
+    return
+
+
+def flatten(nestedArray, result):
+    return
+
+
+def intersection():
+    return
+
+
+def difference(array):
+    return
+
+
+def throttle(func, wait):
+    return
